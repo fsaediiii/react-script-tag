e0a6aff3c562b601ca0a69a4e5ad6628a1897cc4
fatemehsaedi <43900350+fatemehsaedi@users.noreply.github.com>
2021-12-26 15:32:10 +0330

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import { useEffect, useState } from "react";
+import ScriptTag from "react-script-tag";
+
+function App() {
+  const [code, setCode] = useState({});
+
+  const getCode = async () => {
+    await fetch("http://localhost:7501/GetJsData")
+      .then((response) => response.json())
+      .then((data) => setCode(data));
+  };
+
+  useEffect(() => {
+    getCode();
+  }, []);
+
+  return (
+    <>
+      {code && 
+      <ScriptTag type="text/javascript">
+        {
+        `var a=12;
+        $( document ).ready(function() {
+        ${code.jqueryCode}
+        });
+        function runJSCode(){${code.jsCode}}`
+        }
+        </ScriptTag>}
+      <h1>Test for running js and jquery in React</h1>
+      <p>This is only a sample code !!!!!!!</p>
+      <hr/>
+      <button onClick={()=>window.runJSCode()}>Get and run js code!</button>
+      <button>change route!</button>
+    </>
+  );
+}
+
+export default App;
